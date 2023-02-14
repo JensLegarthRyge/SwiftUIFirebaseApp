efe385593dd3fbd94c0d975d7bc831e45ee70920
JensLegarthRyge <jens.legarth.ryge@gmail.com>
2023-02-14 10:22:09 +0100

    Initial Commit

---
+++ SwiftUIFirebaseApp/SwiftUIFirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseAppApp.swift
+//  SwiftUIFirebaseApp
+//
+//  Created by Jens Legarth Ryge on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
